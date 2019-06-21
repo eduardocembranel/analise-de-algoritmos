396fcf710af5c568170856b958c097c91b128835
eduardocembranel <40010629+eduardocembranel@users.noreply.github.com>
2019-06-21 01:23:33 +0000

    Add files via upload

---
+++ geradorMatrizes/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+
+void geraArquivo(int dimensao, std::ofstream &arquivo) {
+   for (int k = 0; k < 2; ++k) {
+      arquivo << "N = " << dimensao << "\n";
+      for (size_t i = dimensao; i > 0; --i) {
+         for (size_t j = dimensao; j > 0; --j) {
+            int num = rand() % 100;
+            if (num < 10) {
+               arquivo << " ";
+            }
+            arquivo << num << " ";
+         }
+         arquivo << "\n";
+      }
+      arquivo << "\n";
+   }
+}
+
+int main() {
+   srand(time(0));
+   std::ofstream arquivo;
+   std::string nomeArquivo;
+   size_t dimensao = 0;
+
+   std::cout << "Nome para o arquivo: ";
+   std::cin >> nomeArquivo;
+   std::cout << "Dimensao para as matrizes: ";
+   std::cin >> dimensao;
+   arquivo.open(nomeArquivo);
+   if (arquivo.is_open()) {
+      std::cout << "Gerando arquivo...\n";
+      geraArquivo(dimensao, arquivo);
+      std::cout << "\nArquivo gerado com sucesso...";
+      arquivo.close();
+   } else {
+      std::cerr << "\nerro inesperado...";
+   }
+   return 0;
+}
